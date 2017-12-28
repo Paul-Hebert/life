62c6bf04680f59aecb6d0af0c807fee484fd5f0a
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-12-27 16:19:31 -0800

    Initial grid rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,71 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var life = {
     dev: true,
-
+    gridData: [],
+    gridHtml: "",
+    startingPopulation: .8,
+    width:30,
+    height:30,
 
     init: function(){
         life.devDebug('life.init');
 
+        life.buildGridData();
+        life.buildGridSkeleton();
+        life.renderGrid();
+
         document.getElementById('startLoop').addEventListener('click', function() { life.loop.start(); });
         document.getElementById('stopLoop').addEventListener('click', function() { life.loop.stop(); });
+    },
+
+    buildGridData(orgs){
+        life.devDebug('life.buildGridData');
+
+        for(var i = 0; i < life.height; i++){
+            var row = [];
+
+            for(var x = 0; x < life.width; x++){
+                row.push(life.buildDot());
+            }
+
+            life.gridData.push(row);
+        }
+
+        life.devDebug(life.gridData, true);
+    },
+
+    buildGridSkeleton(){
+        life.devDebug('life.buildGridSkeleton');
+
+        life.gridHtml = "";
+
+        for(var i = 0; i < life.height; i++){
+            var row = "<div class='row'>";
+
+            for(var x = 0; x < life.width; x++){
+                var liveClass = life.gridData[i][x] ? "alive" : "";
+
+                row += "<span class='dot " + liveClass + "'></span>";
+            }
+
+            row += "</div>";
+
+            life.gridHtml += row;
+        }
+
+        life.devDebug(life.gridHtml);
+    },
+
+    buildDot(){
+        life.devDebug('life.buildDot');
+
+        return Math.random() > life.startingPopulation;
+    },
+
+    renderGrid(){
+        life.devDebug('life.renderGrid');
+
+        document.getElementById('grid').innerHTML = life.gridHtml;
     },
 
     loop: {
@@ -35,15 +93,21 @@ var life = {
 
     turn: function(){
         life.devDebug('life.turn')
-        
+
+        life.renderGrid();
+
         if(life.loop.active){
             requestAnimationFrame(life.turn);
         }
     },
 
-    devDebug: function(message){
+    devDebug: function(message, table){
         if(life.dev){
-            console.log(message);
+            if(table){
+                console.table(message);
+            } else{
+                console.log(message);                
+            }
         }
     }
 }
