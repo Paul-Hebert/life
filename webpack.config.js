534d22fa6f0a02ecf589f57cd8747571c930b8d9
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2018-01-03 18:49:27 -0800

    Added Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ module.exports={
     entry: "./app/index.js",
     output: {
         filename: "bundle.js"
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015"]
+                }
+            }
+        ]
     }
 }
